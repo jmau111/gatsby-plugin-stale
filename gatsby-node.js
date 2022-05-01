794e071b317f17c2ae1ac3eafcbb226626ec1d54
Julien Maury <6878027+jmau111@users.noreply.github.com>
2022-05-01 20:58:32 +0200

    throw error if no url is provided

--- gatsby-node.js
+++ gatsby-node.js
@@ -2,7 +2,9 @@ exports.onCreatePage = ({ page, actions }, pluginOptions) => {
   const { deletePage } = actions
 
   if (pluginOptions.urls === undefined || pluginOptions.urls.length === 0) {
-    return false;
+    const error = 'You are misusing the plugin Gatsby Stale, please provide URLs to remove in the plugin options.';
+    console.error(error);
+    throw new Error(error);
   } 
 
   const urls = pluginOptions.urls;
