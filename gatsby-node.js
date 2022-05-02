253bec8e633edbc5ae752729cb72c156f8338750
Julien Maury <6878027+jmau111@users.noreply.github.com>
2022-05-02 12:35:36 +0200

    take misconfig into account

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,8 +1,14 @@
+const util = require('util');
+
 exports.onCreatePage = ({ page, actions }, pluginOptions) => {
   const { deletePage } = actions
 
-  if (pluginOptions.urls === undefined || pluginOptions.urls.length === 0) {
-    const error = 'You are misusing the plugin Gatsby Stale, please provide URLs to remove in the plugin options.';
+  if (
+    ! util.isArray(pluginOptions.urls) || 
+    pluginOptions.urls === undefined || 
+    pluginOptions.urls.length === 0
+  ) {
+    const error = 'You are misusing the plugin Gatsby Stale, please provide URLs to remove in the plugin options. See https://github.com/jmau111/gatsby-plugin-stale#usage.';
     console.error(error);
     throw new Error(error);
   } 
